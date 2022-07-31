3b70e05692a5da1cbee7525e63ebbc7ad90f6306
xiaoshunshi <song159769@163.com>
2022-07-31 14:52:28 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="left-board">
+      <!-- logo 部分 -->
+       <div class="logo-wrapper">
+        <div class="logo">
+          <img :src="logo" alt="logo"> fly-form-generator
+          <!-- <a class="github" href="https://github.com/JakHuang/form-generator" target="_blank">
+            <img src="https://github.githubassets.com/pinned-octocat.svg" alt>
+          </a> -->
+        </div>
+      </div>
+      <!-- 拖拽菜单 -->
+       <el-scrollbar class="left-scrollbar">
+          <div v-for="(item, listIndex) in leftComponents" :key="listIndex">
+             <div class="components-title">
+              <svg-icon icon-class="component" />
+              {{ item.title }}
+            </div>
+          </div>
+       </el-scrollbar>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import logo from '@/assets/logo.png'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      logo,
+      leftComponents: [
+        {
+          title: '输入型组件'
+          // list: inputComponents
+        },
+        {
+          title: '选择型组件'
+          // list: selectComponents
+        },
+        {
+          title: '布局型组件'
+          // list: layoutComponents
+        }
+      ]
+    }
   }
 }
 </script>
+<style lang='scss'>
+@import '@/styles/home';
+</style>
